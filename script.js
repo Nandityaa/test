b5a8e1465418be6bf09d8f04249d74b4b8fa6c51
Nandityaa <nafananditya2008@gmail.com>
2024-11-06 18:49:23 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const fadeElements = document.querySelectorAll('.scroll-fade-in');
+  
+    const handleScrollAnimation = () => {
+      fadeElements.forEach((element) => {
+        const rect = element.getBoundingClientRect();
+        if (rect.top <= window.innerHeight * 0.9) {
+          element.classList.add('show');
+        }
+      });
+    };
+  
+    window.addEventListener('scroll', handleScrollAnimation);
+    handleScrollAnimation(); // Call on load in case some elements are already in view
+  });
+
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
